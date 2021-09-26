ba704575456f58e80ef139cebb660f16748e2d9d
Indika Kolamba Tantrege <Indika@Mac-mini.local>
2021-09-25 21:21:55 -0400

    change link text to uppercase

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,9 +2,9 @@ const Nav = () => {
   return (
     <nav>
         <ul>
-            <li><a href="http://www.google.ca" title="Opens Google Homepage in a new tab" target="_blank" rel="noreferrer">Home<span>Opens Google homepage in a new tab</span></a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#services">Services</a></li>
+            <li><a href="http://www.google.ca" title="Opens Google Homepage in a new tab" target="_blank" rel="noreferrer">HOME<span>Opens Google homepage in a new tab</span></a></li>
+            <li><a href="#about">ABOUT</a></li>
+            <li><a href="#services">SERVICES</a></li>
         </ul>
     </nav>
   );
