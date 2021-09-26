048bf67383e787c15581ed03da4d8c34f16b0cbe
iamindika <ikolamba2@gmail.com>
2021-09-26 00:59:37 -0400

    define Nav component

---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+const Nav = () => {
+  return (
+    <nav>
+        <ul>
+            <li><a href="http://www.google.ca" title="Opens Google Homepage in a new tab" target="_blank" rel="noreferrer">HOME<span>Opens Google homepage in a new tab</span></a></li>
+            <li><a href="#about">ABOUT</a></li>
+            <li><a href="#services">SERVICES</a></li>
+        </ul>
+    </nav>
+  );
+}
+
+export default Nav;
