7d04f0fc4f0eeff0bb17a8bbfe6ae87b54b58ee2
iamindika <ikolamba2@gmail.com>
2021-09-26 00:55:21 -0400

    define Header component and render to DOM

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Nav from './components/Nav';
+import Header from './components/Header';
 
 ReactDOM.render(
   <React.StrictMode>
     <Nav />
+    <Header />
   </React.StrictMode>,
   document.getElementById('root')
 );
