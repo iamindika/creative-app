9efab9bf945f628c26c0dedf4c03b857b0d48b43
Indika Kolamba Tantrege <Indika@Mac-mini.local>
2021-09-25 20:27:58 -0400

    define Nav starter component and render to DOM

---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+const Nav = () => {
+  return (
+    <nav>
+        <ul>
+            <li><a href="http://www.google.ca" title="Opens Google Homepage in a new tab" target="_blank" rel="noreferrer">Home<span>Opens Google homepage in a new tab</span></a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#services">Services</a></li>
+        </ul>
+    </nav>
+  );
+}
+
+export default Nav;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Nav from './components/Nav';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Nav />
   </React.StrictMode>,
   document.getElementById('root')
 );
