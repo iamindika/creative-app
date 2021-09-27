f56c3cb713e1437ffc1dd0213e5f40f78dee507f
iamindika <ikolamba2@gmail.com>
2021-09-27 19:56:43 -0400

    export Services component

---
+++ src/components/Services.js
@@ -0,0 +1,13 @@
+const Services = () => {
+  return (
+    <section id="services">
+      <h3>Services</h3>
+      <div>
+        <img src="https://picsum.photos/200/200" alt="Picsum placeholder photo" />
+        <p>We offer all the services, all of the services it's so crazy. Look at this bird that we crafted. We are so crafty. So creative.</p>
+      </div>
+    </section>
+  )
+}
+
+export default Services;
