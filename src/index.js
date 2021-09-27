51244f35fe7936c28a47ec31a80d9e383a321eca
iamindika <ikolamba2@gmail.com>
2021-09-27 19:57:33 -0400

    import and render Services component

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Nav from './components/Nav';
 import Header from './components/Header';
+import Services from './components/Services';
 
 ReactDOM.render(
   <React.StrictMode>
     <Nav />
     <Header />
+    <Services />
   </React.StrictMode>,
   document.getElementById('root')
 );
