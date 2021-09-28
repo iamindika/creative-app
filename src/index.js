689f11eed38b4c6880cfecdb1c5c788358d09d8e
iamindika <ikolamba2@gmail.com>
2021-09-27 21:24:11 -0400

    import and render About component

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import Nav from './components/Nav';
 import Header from './components/Header';
 import Services from './components/Services';
+import About from './components/About';
 
 ReactDOM.render(
   <React.StrictMode>
     <Nav />
     <Header />
     <Services />
+    <About />
   </React.StrictMode>,
   document.getElementById('root')
 );
