4d3fc267821ad1fb4ba50fb2f7408d42a9ec39ae
iamindika <ikolamba2@gmail.com>
2021-09-27 21:13:49 -0400

    set h3 to all-caps and change size of img placeholder

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,9 +1,9 @@
 const Services = () => {
   return (
     <section id="services">
-      <h3>Services</h3>
+      <h3>SERVICES</h3>
       <div>
-        <img src="https://picsum.photos/200/200" alt="Picsum placeholder photo" />
+        <img src="https://picsum.photos/600/600" alt="Picsum placeholder photo" />
         <p>We offer all the services, all of the services it's so crazy. Look at this bird that we crafted. We are so crafty. So creative.</p>
       </div>
     </section>
