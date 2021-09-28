94c8cf7507a9bbb01c88409416f178a52fcf27f4
iamindika <ikolamba2@gmail.com>
2021-09-27 21:23:44 -0400

    define About component

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+const About = () => {
+  return (
+    <section id="about">
+      <h3>ABOUT</h3>
+      <div>
+        <img src="https://picsum.photos/600/600" alt="Picsum placeholder photo"/>
+        <p>We are just so creative. Have you seen the boat up there that we created. Just so wow. We love being creative and crafting. Have we mentioned that?</p>
+      </div>
+    </section>
+  );
+}
+
+export default About;
